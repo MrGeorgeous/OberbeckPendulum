36517f38c10a98422dc36827914f2b4c4febea1c
George Semenov <george.sem@live.ru>
2020-04-11 17:24:27 +0300

    Update Arduino.c

--- Arduino.c
+++ Arduino.c
@@ -1,4 +1,3 @@
-
 typedef const uint8_t pinPort;
 typedef double waveTime; // time in seconds
 typedef double microWaveTime; // time in microseconds 10^-6
@@ -9,18 +8,24 @@ typedef double cmDistance; // distance in cm 10^-2
 typedef double velocity; // velocity in m/s
 
 float temperature = 24.0; // room temperature between 0 C and 30 C
-unsigned int serialSpeed = 57600; // bps data speed for COM-interaction
+unsigned int serialSpeed = 9600; // bps data speed for COM-interaction
 integerMilliWaveTime serialTimeout = 50; // milliseconds
-integerMilliWaveTime pulseInTimeout = 50; // milliseconds
+integerMicroWaveTime pulseInTimeout = 10000; // microseconds
 integerMilliWaveTime dataFrequency = 100; // milliseconds for the delay for measurements
-cmDistance betweenSensorAndStart = 2.7;
-cmDistance betweenSensorAndFinish = 2.6;
-cmDistance distanceInOperation = 70.0;
-pinPort sensorStartEcho = 2;
-pinPort sensorStartTrig = 3;
-pinPort sensorFinishEcho = 4;
-pinPort sensorFinishTrig = 5;
 
+size_t measurementsPerTact = 3;
+integerMicroWaveTime pauseDurationPerTact = 3;
+integerMicroWaveTime transmitDurationPerTact = 5;
+
+cmDistance betweenSensorAndStart = 12.7;
+cmDistance betweenSensorAndFinish = 12.6;
+cmDistance distanceInOperation = 70.0;
+
+pinPort sensorStartEcho = 9;
+pinPort sensorStartTrig = 8;
+pinPort sensorFinishEcho = 11;
+pinPort sensorFinishTrig = 7;
+cmDistance sensorThreshold = 40.0;
 
 velocity speed_of_sound = 331.5 + 0.59 * temperature; // speed of sound for 24 C
 integerMicroWaveTime noWaveCollisionsInterval = 1000 + 1000 * 10 / speed_of_sound * (distanceInOperation + betweenSensorAndStart + betweenSensorAndFinish); 
@@ -54,9 +59,9 @@ microWaveTime UltrasonicSensor_getWaveTime(struct UltrasonicSensor p) {
 cmDistance UltrasonicSensor_getDistance(struct UltrasonicSensor p, const size_t totalMeasurements = 3,
                     integerMicroWaveTime pauseDuration = 3, integerMicroWaveTime transmitDuration = 5) {
                       
-  UltrasonicSensor_sendWave(p, pauseDuration, transmitDuration);
   microWaveTime count = 0.0;
   for (size_t i = 0; i < totalMeasurements; i++) {
+      UltrasonicSensor_sendWave(p, pauseDuration, transmitDuration);
       count += UltrasonicSensor_getWaveTime(p);
   }
   microWaveTime avg_time = count / totalMeasurements;
@@ -64,8 +69,6 @@ cmDistance UltrasonicSensor_getDistance(struct UltrasonicSensor p, const size_t 
   return d;
   
 }
-
-
 
 struct UltrasonicSensor sensorStart = {sensorStartEcho, sensorStartTrig};
 struct UltrasonicSensor sensorFinish = {sensorFinishEcho, sensorFinishTrig};
@@ -93,32 +96,62 @@ void serialEvent() {
 
     if (msg.startsWith("params")) {
        Serial.println(" <<< OBERBECK PENDULUM >>> ");
-       Serial.print("temperature: "); Serial.print(temperature); Serial.println(" C");
+       Serial.println(" === COM settings === ");
        Serial.print("serialSpeed: "); Serial.print(serialSpeed); Serial.println(" bps");
-       Serial.print("serialTimeout: "); Serial.print(serialTimeout); Serial.println(" milliseconds");
-       Serial.print("pulseInTimeout: "); Serial.print(pulseInTimeout); Serial.println(" milliseconds");
-       Serial.print("dataFrequency: "); Serial.print(dataFrequency); Serial.println(" milliseconds");
+       Serial.print("serialTimeout: "); Serial.print(serialTimeout); Serial.println(" microseconds");
+       Serial.println(" === Physical constants === ");
+       Serial.print("speed_of_sound: "); Serial.print(speed_of_sound); Serial.println(" m/s");
+       Serial.print("noWaveCollisionsInterval: "); Serial.print(round(noWaveCollisionsInterval / 1000)); Serial.println(" milliseconds");
+       Serial.println(" === Environmental constants === ");
+       Serial.print("temperature: "); Serial.print(temperature); Serial.println(" C");
        Serial.print("betweenSensorAndStart: "); Serial.print(betweenSensorAndStart); Serial.println(" cm");
        Serial.print("betweenSensorAndFinish: "); Serial.print(betweenSensorAndFinish); Serial.println(" cm");
        Serial.print("distanceInOperation: "); Serial.print(distanceInOperation); Serial.println(" cm");
+       Serial.println(" === Measurement settings === ");
+       Serial.print("dataFrequency: "); Serial.print(dataFrequency); Serial.println(" milliseconds");
+       Serial.print("measurementsPerTact: "); Serial.print(measurementsPerTact); Serial.println(" times");
+       Serial.print("pauseDurationPerTact: "); Serial.print(pauseDurationPerTact); Serial.println(" microseconds");
+       Serial.print("transmitDurationPerTact: "); Serial.print(transmitDurationPerTact); Serial.println(" microseconds");
+       Serial.println(" === Sensor settings === ");
        Serial.print("sensorStartEcho: "); Serial.print(sensorStartEcho); Serial.println(" pin");
        Serial.print("sensorStartTrig: "); Serial.print(sensorStartTrig); Serial.println(" pin");
        Serial.print("sensorFinishEcho: "); Serial.print(sensorFinishEcho); Serial.println(" pin");
        Serial.print("sensorFinishTrig: "); Serial.print(sensorFinishTrig); Serial.println(" pin");
-       Serial.print("speed_of_sound: "); Serial.print(speed_of_sound); Serial.println(" m/s");
-       Serial.print("noWaveCollisionsInterval: "); Serial.print(round(noWaveCollisionsInterval / 1000)); Serial.println(" milliseconds");
+       Serial.print("pulseInTimeout: "); Serial.print(pulseInTimeout); Serial.println(" microseconds");
+    
     }
     
+}
+
+
+cmDistance analysis(cmDistance x_1, cmDistance x_2) {
+
+  if ((x_1 <= sensorThreshold) && (x_2 <= sensorThreshold)) {
+    return (x_1 + x_2) / 2;
+  }
+
+  if (!(x_1 <= sensorThreshold) && (x_2 <= sensorThreshold)) {
+    return x_2;
+  }
+
+  if ((x_1 <= sensorThreshold) && !(x_2 <= sensorThreshold)) {
+    return x_1;
+  }
+
+  if (!(x_1 <= sensorThreshold) && !(x_2 <= sensorThreshold)) {
+    return 0.0;
+  }
+  
 }
 
 void loop(){
 
   delay(round(noWaveCollisionsInterval / 1000));
-  cmDistance x_1 = UltrasonicSensor_getDistance(sensorStart, 3, 3, 5) - betweenSensorAndStart;
+  cmDistance x_1 = UltrasonicSensor_getDistance(sensorStart, measurementsPerTact, pauseDurationPerTact, transmitDurationPerTact) - betweenSensorAndStart;
   delay(round(noWaveCollisionsInterval / 1000));
-  cmDistance x_2 = distanceInOperation - UltrasonicSensor_getDistance(sensorFinish, 3, 3, 5) - betweenSensorAndFinish;
+  cmDistance x_2 = distanceInOperation - UltrasonicSensor_getDistance(sensorStart, measurementsPerTact, pauseDurationPerTact, transmitDurationPerTact) - betweenSensorAndFinish;
 
-  cmDistance x_avg = (x_1 + x_2) / 2.0;
+  cmDistance heuristics = (x_1 + x_2) / 2.0;
 
   Serial.println(x_avg);
   delay(dataFrequency);
